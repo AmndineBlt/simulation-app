dbbbb2930488d4f04b35fba775484c99d9215f92
AmndineBlt <amandine.bluteau@oclock.school>
2026-04-26 14:28:35 +0200

    update: recharger la simulation à chaque connection

--- api/src/simulation/gateway/simulation.gateway.ts
+++ api/src/simulation/gateway/simulation.gateway.ts
@@ -1,11 +1,11 @@
-import { OnGatewayInit, WebSocketGateway, WebSocketServer, } from "@nestjs/websockets";
-import { Server } from "socket.io";
+import { OnGatewayConnection, OnGatewayInit, WebSocketGateway, WebSocketServer, } from "@nestjs/websockets";
+import { Server, Socket } from "socket.io";
 import { SimulationService } from "../simulation.service";
 import { setInterval } from "node:timers";
 import { Grid } from "../types/grid.type";
 
 @WebSocketGateway({ cors: { origin: "*" } })
-export class SimulationGateway implements OnGatewayInit {
+export class SimulationGateway implements OnGatewayInit, OnGatewayConnection {
   @WebSocketServer()
   server: Server;
 
@@ -17,10 +17,14 @@ export class SimulationGateway implements OnGatewayInit {
   }
 
   startSimulation() {
-    this.simulationService.init();
     setInterval(() => {
       const grid: Grid = this.simulationService.tick();
       this.server.emit("grid", grid);
     }, 1000);
+  }
+
+  handleConnection(client: Socket) {
+    console.log("Client connecté !");
+    this.simulationService.init(); // réinitialise à chaque connexion
   }
 }
