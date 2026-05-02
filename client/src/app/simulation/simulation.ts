1bce1ce0ba143d283e4853fbf4116e1de9f00629
AmndineBlt <amandine.bluteau@oclock.school>
2026-05-02 13:24:20 +0200

    add: ajout de la courbe d'evolution de la population de loup, lapin et carotte

--- client/src/app/simulation/simulation.ts
+++ client/src/app/simulation/simulation.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, signal } from "@angular/core";
 import { io } from "socket.io-client";
 import { NgClass } from "@angular/common";
+import { PopulationChart } from "../population-chart/population-chart";
 
 interface GridCell {
   diet?: string;
@@ -9,18 +10,33 @@ interface GridCell {
 
 @Component({
   selector: "app-simulation",
-  imports: [NgClass],
+  imports: [NgClass, PopulationChart],
   templateUrl: "./simulation.html",
   styleUrl: "./simulation.css",
 })
 export class Simulation implements OnInit {
   grid = signal<(GridCell | null)[][]>([]);
+  rabbitHistory = signal<number[]>([]);
+  wolfHistory = signal<number[]>([]);
+  carrotHistory = signal<number[]>([]);
 
   ngOnInit() {
     const socket = io("http://localhost:3000");
 
     socket.on("grid", (grid: (GridCell | null)[][]) => {
-      this.grid.set(grid); // met à jour le signal
+      this.grid.set(grid);
+
+      const flat = grid.flat();
+
+      const rabbits = flat.filter((cell) => cell?.type === "rabbit").length;
+      const wolves = flat.filter((cell) => cell?.type === "wolf").length;
+      const carrots = flat.filter((cell) => cell?.type === "carrot").length;
+
+      this.rabbitHistory.update((history) => [...history, rabbits]);
+      this.wolfHistory.update((history) => [...history, wolves]);
+      this.carrotHistory.update((history) => [...history, carrots]);
+
+      console.log("rabbitHistory:", this.rabbitHistory());
     });
   }
 }
